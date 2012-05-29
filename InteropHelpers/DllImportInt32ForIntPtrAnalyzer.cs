859fc6a3e7d1985d58443ac6ac58b90bbaa0635c
Kevin Jones <kevin@vcsjones.com>
2012-05-29 10:00:06 -0400

    Refactored common problem analysis for interop into a base.

--- InteropHelpers/DllImportInt32ForIntPtrAnalyzer.cs
+++ InteropHelpers/DllImportInt32ForIntPtrAnalyzer.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Linq;
-using JetBrains.ReSharper.Daemon.CSharp.Stages;
+﻿using JetBrains.ReSharper.Daemon.CSharp.Stages;
 using JetBrains.ReSharper.Daemon.Stages;
 using JetBrains.ReSharper.Daemon.Stages.Dispatcher;
 using JetBrains.ReSharper.Psi;
@@ -9,35 +7,13 @@ using JetBrains.ReSharper.Psi.CSharp.Tree;
 namespace InteropHelpers
 {
     [ElementProblemAnalyzer(new[] { typeof(IMethodDeclaration) }, HighlightingTypes = new[] { typeof(DllImportMissingHighlighting) })]
-    public class DllImportInt32ForIntPtrAnalyzer : ElementProblemAnalyzer<IMethodDeclaration>
+    public class DllImportInt32ForIntPtrAnalyzer : DllImportMethodProblemAnalyzer
     {
         private static readonly ClrTypeName IntPtrClrType = new ClrTypeName("System.IntPtr");
-        protected override void Run(IMethodDeclaration element, ElementProblemAnalyzerData data, IHighlightingConsumer consumer)
+        protected override void Run(IDllImportMethodDeclaration importMethod, ElementProblemAnalyzerData data, IHighlightingConsumer consumer)
         {
-            Func<IAttribute, bool> isDllImportAttribute = a =>
-            {
-                var typeReference = a.TypeReference;
-                if (typeReference != null)
-                {
-                    var typeElement = typeReference.Resolve().DeclaredElement as ITypeElement;
-                    if (typeElement != null && Equals(typeElement.GetClrName(), DllImportMissingAnalyzer.DllImportAttribute))
-                    {
-                        return true;
-                    }
-                }
-                return false;
-            };
-            var attributes = element.Attributes;
-            var dllImportAttribute = attributes.FirstOrDefault(isDllImportAttribute);
-            if (!element.IsExtern || !element.IsStatic || dllImportAttribute == null || dllImportAttribute.ConstructorArgumentExpressions.Count == 0)
-            {
-                return;
-            }
-            var libraryName = dllImportAttribute.ConstructorArgumentExpressions[0].ConstantValue.Value as string;
-            if (libraryName == null)
-            {
-                return;
-            }
+            var element = importMethod.MethodDeclaration;
+            var libraryName = importMethod.ImportedDll;
             var factory = new LibraryFactory();
             var library = factory.LoadLibrary(libraryName);
             var export = library.GetExport(element.NameIdentifier.Name);
---
+++ InteropHelpers/DllImportMethodProblemAnalyzer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using JetBrains.ReSharper.Daemon.Stages;
+using JetBrains.ReSharper.Daemon.Stages.Dispatcher;
+using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.Psi.CSharp.Tree;
+
+namespace InteropHelpers
+{
+    public abstract class DllImportMethodProblemAnalyzer : ElementProblemAnalyzer<IMethodDeclaration>
+    {
+        private static readonly ClrTypeName DllImportAttribute = new ClrTypeName("System.Runtime.InteropServices.DllImportAttribute");
+        protected sealed override void Run(IMethodDeclaration element, ElementProblemAnalyzerData data, IHighlightingConsumer consumer)
+        {
+            Func<IAttribute, bool> isDllImportAttribute = a =>
+            {
+                var typeReference = a.TypeReference;
+                if (typeReference != null)
+                {
+                    var typeElement = typeReference.Resolve().DeclaredElement as ITypeElement;
+                    if (typeElement != null && Equals(typeElement.GetClrName(), DllImportAttribute))
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            };
+            var attributes = element.Attributes;
+            var dllImportAttribute = attributes.FirstOrDefault(isDllImportAttribute);
+            if (!element.IsExtern || !element.IsStatic || dllImportAttribute == null || dllImportAttribute.ConstructorArgumentExpressions.Count == 0)
+            {
+                return;
+            }
+            var libraryName = dllImportAttribute.ConstructorArgumentExpressions[0].ConstantValue.Value as string;
+            if (libraryName == null)
+            {
+                return;
+            }
+            Run(new DllImportMethodDeclarationImpl {ImportedDll = libraryName, MethodDeclaration = element}, data, consumer);
+        }
+
+        protected abstract void Run(IDllImportMethodDeclaration importMethod, ElementProblemAnalyzerData data, IHighlightingConsumer consumer);
+
+        private class DllImportMethodDeclarationImpl : IDllImportMethodDeclaration
+        {
+            public IMethodDeclaration MethodDeclaration { get; set; }
+            public string ImportedDll { get; set; }
+        }
+    }
+
+    public interface IDllImportMethodDeclaration
+    {
+        IMethodDeclaration MethodDeclaration { get; }
+        string ImportedDll { get;  }
+    }
+}
